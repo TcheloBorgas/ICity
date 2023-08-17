9856ba800c95105516f737eb95afde2a3f12ce15
Marcelo Henrique Borgas <marcelo.borgas@edu.fecap.br>
2023-08-16 21:44:26 -0300

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -194,5 +194,3 @@ resource "aws_cloudtrail" "cloudtrail" {
   s3_bucket_name                = aws_s3_bucket.image_bucket.id
   include_global_service_events = true
 }
-
-# ... Adicione outras configurações conforme necessário ...
